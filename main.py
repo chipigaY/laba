548a88a0cf3e7b0c57074b9732070edc52c75048
1 <chipiga2005@gmail.com>
2024-11-05 01:48:49 +0300

    lab 2

--- main.py
+++ main.py
@@ -9,3 +9,20 @@ n = input("Введите строку: ")
 count = F(n)
 print(f"Количество чисел в строке: {count}")
 
+new_n = ""
+flag = True
+for x in n:
+    if x == '(':
+        flag = False
+    elif x == ')':
+        flag = True
+    elif flag:
+        new_n += x
+        print("Исходная строка без символов между скобками:",'(',new_n,')')
+n = len(new_n)
+lst = [int(new_n) for _ in range(n)]
+count_zero = lst.count(0)
+print("Количество элементов списка равных 0:", count_zero)
+min_elem = min(lst)
+sum_after_min = sum(lst[lst.index(min_elem) + 1:])
+print("Сумма элементов списка после минимального элемента:", sum_after_min)
